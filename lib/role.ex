77ddd4e4f350851536e95baf76dd3b01a270d38c
Ivan Kozik <ivan@ludios.org>
2017-12-25 08:19:48 +0000

    Add support for setting vfio-pci.ids= cmdline using vfio_pci_id:ID tags

--- lib/role.ex
+++ lib/role.ex
@@ -1,17 +1,22 @@
+alias Converge.Util
+
 defmodule RoleVfio do
-	def role(_tags \\ []) do
+	def role(tags \\ []) do
 		%{
 			cmdline_normal_only: [
 				# pcie_acs_override= requires the ACS override patch; we use it to put
 				# our IOMMU Group 1 GPUs into their own groups.
 				"pcie_acs_override=downstream",
-			]
+			] ++ case Util.tag_values(tags, "vfio_pci_id") do
+				[]  -> []
+				ids -> ["vfio-pci.ids=#{Enum.join(ids, ",")}"]
+			end,
 			boot_time_kernel_modules: [
 				"vfio",
 				"vfio_iommu_type1",
 				"vfio_pci",
 				"vfio_virqfd",
-			]
+			],
 			implied_roles: [RoleIntelIommu],
 		}
 	end
